c0a53fdd5016f3e2166c63e1fb561a9d68113215
Tugolbai Maksatbek uulu <mkstbkv751@gmail.com>
2022-04-05 03:44:35 +0600

    роут для артиста

--- src/artists/artists.controller.ts
+++ src/artists/artists.controller.ts
@@ -1,4 +1,63 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { Artist, ArtistDocument } from '../schemas/artist.schema';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { CreateArtistDto } from './create-artist.dto';
+import { Album, AlbumDocument } from '../schemas/album.schema';
+import { Track } from '../schemas/track.schema';
 
 @Controller('artists')
-export class ArtistsController {}
+export class ArtistsController {
+  constructor(
+    @InjectModel(Artist.name) private artistModel: Model<ArtistDocument>,
+    @InjectModel(Album.name) private albumModel: Model<AlbumDocument>,
+    @InjectModel(Track.name) private trackModel: Model<ArtistDocument>,
+  ) {}
+
+  @Get()
+  getAll() {
+    return this.artistModel.find();
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: string) {
+    return this.artistModel.findById(id);
+  }
+
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('image', { dest: './public/uploads/artists' }),
+  )
+  create(
+    @UploadedFile() file: Express.Multer.File,
+    @Body() artistData: CreateArtistDto,
+  ) {
+    const artist = new this.artistModel({
+      name: artistData.name,
+      info: artistData.info,
+      image: file ? '/uploads/artists/' + file.filename : null,
+    });
+
+    return artist.save();
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: string) {
+    const albums = await this.albumModel.find({ artist: id });
+    await this.artistModel.deleteOne({ _id: id });
+    await this.albumModel.deleteMany({ artist: id });
+    await this.trackModel.deleteMany({ album: { $in: albums } });
+
+    return { message: 'Deleted!' };
+  }
+}
